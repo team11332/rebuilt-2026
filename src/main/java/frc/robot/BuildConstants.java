1263491b13630d40d1b79aa4d304a4971ddcb66e
yahavattias <yahav.attiasl@gmail.com>
2026-06-28 23:28:49 +0300

    added new file

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "rebuilt-2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "e8ea17ee9f6c32db006dde043c2e5fe74b818ea5";
+  public static final String GIT_DATE = "2026-06-25 10:18:54 GMT-04:00";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-06-25 10:19:35 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1782397175671L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
